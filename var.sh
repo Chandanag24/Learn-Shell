51a905ed2106192571e72a0f7e60aec2a67e97dd
chandana24 <gantachandana98@gmail.com>
2023-08-28 14:56:56 +0530

    Password NFR

---
+++ var.sh
@@ -0,0 +1,9 @@
+#special_variables
+#$0,$N,$*,$#
+#substitution variables
+##command substitution
+DATE=$(date)
+echo today date is $(DATE)
+#Arthematic substitution
+ADD=$((2+2))
+echo Add of 2+2 is $((ADD))
